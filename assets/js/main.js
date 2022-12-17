637e4b3e08ba4871176b52c28de3f5a6b3a55c16
Gleidson Lira <lira.spfc@gmail.com>
2022-12-17 09:02:23 -0300

    Adicionando mai linhas ao Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 const inputs = document.getElementsByTagName('input');
+
+function validadeInputs() {
+    for (let i=0; i< inputs.length; i++) {
+        inputs[i].addEventListener('blur', function () {
+            const erro = document.getElementsByClassName('error');
+            const erroIcon = document.getElementsByClassName('error__icon');
+        })
+    }
+}
