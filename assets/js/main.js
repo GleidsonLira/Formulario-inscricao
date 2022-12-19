8bc54715c3a20cb9991b2de50c390f22363e82c1
Gleidson Lira <lira.spfc@gmail.com>
2022-12-19 11:11:37 -0300

    Pequenas alterações na declaração de classes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ function validadeInputs() {
     for (let i=0; i< inputs.length; i++) {
         inputs[i].addEventListener('blur', function () {
             const erro = document.getElementsByClassName('error');
-            const erroIcon = document.getElementsByClassName('error__icon');
+            const erroIcon = document.getElementsByClassName('error-icon');
 
             if (!this.valeu) {
                 this.classList.add('invalid');
