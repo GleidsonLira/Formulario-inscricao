89fcb4b589ee541b8fdabd332bcc90546188e1a7
Gleidson Lira <lira.spfc@gmail.com>
2022-12-22 10:59:38 -0300

    Novas adições no código Javascript

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,19 @@ const lastName = document.getElementById('sobrenome')
 const emailAddress = document.getElementById('email')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => submit(e))
+
+function submit(event) {
+    if (checkEmpty([firstName, lastName, password])) {
+        event.preventDefault()
+    }
+    if (checkEmpty([emailAddress]) || !isEmail(emailAddress)) {
+        event.preventDefault()
+        showErrorMessage(emailAddress, true)
+    }
+    else {
+        showErrorMessage(emailAddress, false)
+    }
+}
+
