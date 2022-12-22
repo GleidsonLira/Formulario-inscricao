5035b1c85e8a7ad584eb55a9e1a0fce24a8a17d8
Gleidson Lira <lira.spfc@gmail.com>
2022-12-22 10:53:19 -0300

    Reestruturando Javascript

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,22 +1,6 @@
-const inputs = document.getElementsByTagName('input');
-
-function validateInputs() {
-    for (let i=0; i< inputs.length; i++) {
-        inputs[i].addEventListener('blur', function () {
-            const error = document.getElementsByClassName('error');
-            const errorIcon = document.getElementsByClassName('error-icon');
-            
-            if (!this.value) {
-                this.classList.add('invalid');
-                error[i].classList.add('visible');
-                errorIcon[i].classList.add('visible');
-            } else {
-                this.classList.remove('invalid');
-                error[i].classList.remove('visible');
-                errorIcon[i].classList.remove('visible');
-            }
-        })
-    }
-}
-
-validateInputs();
+const submitButton = document.getElementById('botaoverde')
+const firstName = document.getElementById('nome')
+const lastName = document.getElementById('sobrenome')
+const emailAddress = document.getElementById('email')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
